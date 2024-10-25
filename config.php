8a990734bbfb2daccadca0231918b5b9af010066
rherison <rakotondrasoaherisonsafidy@gmail.com>
2024-10-25 11:02:30 +0200

    Updating

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+
+$faker = Faker\Factory::create();
+
+$host = 'localhost';
+$port = '3306';
+$db = 'e-commerce-sql';
+$user = 'root';
+$pass = '';
+$pdo = null;
+?>
